2a35f2586656e7ac5e136726b621b04a2c7b725d
Arnshere <arnob2779.sarker@gmail.com>
2026-05-28 21:45:22 +0600

    Add files via upload

---
+++ python.py
@@ -0,0 +1,23 @@
+print("arjun")
+age=12
+is_student=true
+weight=38.5
+
+print("name :", name)
+print("data type of name is",type (name))
+print("age:",age)
+print("data type of name is",type (age))
+print("is_student :", student)
+print("data type of name is", type (student))
+print("weight :", weight)
+print("data type of name is",type(weight) )
+
+print("\n after type casting.....")
+age=str(age)
+print(age)
+print("data type of name is", type(age))
+weight=int(weight)
+print(weight)
+print("data type of name is", type(weight) )
+
+
