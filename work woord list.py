cefe1291d291fd9c470b73a97e214da5115b7036
Arnshere <arnob2779.sarker@gmail.com>
2026-06-04 21:54:09 +0600

    Add files via upload

---
+++ work woord list.py
@@ -0,0 +1,23 @@
+1st = ['apple', 'guava', 'mango', 'banana', 'kiwi']
+
+print("lenth of list:", len(1st))
+print("first element:", 1st[0])
+print("last element:", 1st[-1])
+
+1st.append('papaya')
+print("updated list :", 1st)
+
+1st.remove('guava')
+print("updated list :", 1st)
+
+
+1st.sort()
+print("sorted list :", 1st)
+
+1st.pop()
+print("sorted list :", 1st)
+
+
+
+
+
