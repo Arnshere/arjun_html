0b824032012715d5915709835a728b26a6ba2461
Arnshere <arnob2779.sarker@gmail.com>
2026-06-04 21:19:22 +0600

    Add files via upload

---
+++ odd and even number.py
@@ -0,0 +1,19 @@
+number =int(input("enter number to check"))
+print("number to be checked :", number)
+
+
+if number>0:
+    print("this is a positive number")
+
+
+elif number<0:
+    print("this is a negative number")
+
+
+else:
+    print("this is neutral")
+
+
+
+
+
