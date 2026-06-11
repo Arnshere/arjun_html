2c962b059a992ca7c18a04c3f780a7f60c116b19
Arnshere <arnob2779.sarker@gmail.com>
2026-06-11 21:45:11 +0600

    Add files via upload

---
+++ abousuloute.py
@@ -0,0 +1,22 @@
+def absolutue_value(num):
+    
+    
+    
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+    
+    
+    
+print("Absoulute value of 89 :", absoulute_value(89))
+
+print("Absoulute value of -89 :", absoulute_value(-89))
+
+
+
+
+
+
+
